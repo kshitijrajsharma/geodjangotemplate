4a2b0f99d5b097e5e974f018f0a277e863aaf47a
Kshitij Raj Sharma <36752999+itskshitiz321@users.noreply.github.com>
2021-06-23 17:03:12 +0545

    Update urls.py
    added swagger url

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views as core_views
+from rest_framework_swagger.views import get_swagger_view
 
+schema_view = get_swagger_view(title='GeoDjango API')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',schema_view),
     path('api/location/',core_views.locationapi),
 ]
